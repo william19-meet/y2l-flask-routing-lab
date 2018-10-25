a39d8fd30207be07ef23d9e61a633de99f2c548b
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:44:09 +0300

    WE BACK BABY

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template('home_page.html')
+@app.route('/shop')
+def shop_page():
+	shows = ["The Flash 10$.", "Rick and Morty 20$", "Lcs (jk) free"]
+	return render_template('shop.html', shows=shows)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
